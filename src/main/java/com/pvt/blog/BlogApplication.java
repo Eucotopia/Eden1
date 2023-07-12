590a9039d2507ef465a56d01a83f0a3f67f06a59
码农一号 <94905769+lw3499508634@users.noreply.github.com>
2023-07-12 23:31:00 +0800

    Initial commit

---
+++ src/main/java/com/pvt/blog/BlogApplication.java
@@ -0,0 +1,16 @@
+package com.pvt.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author LW
+ */
+@SpringBootApplication
+public class BlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogApplication.class, args);
+    }
+
+}
