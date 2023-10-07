5c841374b00102b55df451967fc5874516833821
A better tomorrow <3499508634@qq.com>
2023-10-07 18:23:13 +0800

    1

--- src/main/java/com/pvt/blog/controller/ArticleController.java
+++ src/main/java/com/pvt/blog/controller/ArticleController.java
@@ -1,2 +1,13 @@
-package com.pvt.blog.controller;public class ArticleController {
+package com.pvt.blog.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author LIWEI
+ */
+@RequestMapping("/article")
+@RestController
+public class ArticleController {
+
 }
