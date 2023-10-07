3d6b89f8f9d560c089b6364e7e9d9a80920fa175
A better tomorrow <3499508634@qq.com>
2023-10-07 17:40:49 +0800

    添加README.md

--- blog.sql
+++ blog.sql
@@ -18,7 +18,8 @@
 --
 -- Table structure for table `articles`
 --
-
+create database blog;
+use blog;
 DROP TABLE IF EXISTS `articles`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
